e43fb6e5120c81436dea6982bdabdd55b26debbb
Vurv <56230599+Vurv78@users.noreply.github.com>
2021-08-21 21:19:13 -0700

    Have preprocessor work with compile error traces
    Same as E2, just replaces the comment/directives with whitespace.

--- src/Base/Preprocessor.hx
+++ src/Base/Preprocessor.hx
@@ -1,5 +1,9 @@
 package base;
+using hx.strings.Strings;
 
+private function FILL_WHITESPACE(x: EReg) {
+	return ' '.repeat(x.matched(0).length);
+}
 
 /**
  * Preprocessor class
@@ -7,20 +11,22 @@ package base;
  * You can modify the regexes to your needs but by default will remove comments & directives.
  */
 class Preprocessor {
-	var repl: Map<EReg, String>;
+	var repl: Map<EReg, (EReg)->String>;
+
 	public function process(script: String) {
 		var processed = script;
+
 		for (regex => with in repl)
-			processed = regex.replace(processed, "");
+			processed = regex.map( processed, with );
 
 		return processed;
 	}
 
 	public function new() {
 		final repl = [
-			~/#[^\n]*/g => "", // Single-line comment
-			~/#\[[\s\S]*\]#/g => "", // Multiline
-			~/@[^\n]*/g => "" // Directive
+			~/#\[[\s\S]*\]#/g => FILL_WHITESPACE, // Single-line comment
+			~/#[^\n]*/g => FILL_WHITESPACE, // Multiline
+			~/@[^\n]*/g => FILL_WHITESPACE // Directive
 		];
 		this.repl = repl;
 	}
